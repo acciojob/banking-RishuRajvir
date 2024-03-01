ded119826edcedbd588f7ad6750d92872d3d9a54
Rishu Raj <rishu25dec@gmail.com>
2024-03-01 20:46:11 +0530

    5th commit

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -1,15 +1,17 @@
 package com.driver;
 
+import static java.lang.String.valueOf;
+
 public class CurrentAccount extends BankAccount{
     String tradeLicenseId; //consists of Uppercase English characters only
 
-    public String getTradeLicenseId(){
+    public String getTradeLicenseId() {
         return tradeLicenseId;
     }
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
-        super(name,balance,5000);
+        super(name, balance, 5000);
         this.tradeLicenseId = tradeLicenseId;
         if(balance < 5000){
             throw new Exception("Insufficient Balance");
@@ -22,6 +24,7 @@ public class CurrentAccount extends BankAccount{
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
+
         if(!isNumberValid(tradeLicenseId)){
             String rearrangedId = arrangeString(tradeLicenseId);
             if(rearrangedId == ""){
@@ -61,6 +64,30 @@ public class CurrentAccount extends BankAccount{
             return "";
         }
 
+//        int index = 0;
+//        char[]res = new char[n];
+//        for(index=0;index<n;index=index+2){
+//            if(count[maxCount]>0){
+//                res[index] = ch_max;
+//                count[maxCount]--;
+//            }else{
+//                break;
+//            }
+//        }
+//
+//        for(int i=0;i<26;i++){
+//            char ch = (char)('A' + i);
+//            while(count[i] > 0){
+//                if(index>n){
+//                    index = 1;
+//                }
+//                res[index] = ch;
+//                index = index + 2;
+//                count[i]--;
+//            }
+//        }
+//        String ans = valueOf(res);
+//        return ans;
 
         String res = "";
         for (int i = 0; i < n; i++) {
@@ -102,4 +129,3 @@ public class CurrentAccount extends BankAccount{
     }
 
 }
-
--- src/main/java/com/driver/StudentAccount.java
+++ src/main/java/com/driver/StudentAccount.java
@@ -6,10 +6,15 @@ public class StudentAccount extends BankAccount{
 
     public StudentAccount(String name, double balance, String  institutionName) {
         //minimum balance is 0 by default
-        super(name,balance,0);
+        super(name, balance, 0);
         this.institutionName = institutionName;
     }
-    public  String getInstitutionName() {
+
+    // public void setInstitutionName(String institutionName) {
+    //     this.institutionName = institutionName;
+    // }
+
+    public String getInstitutionName() {
         return institutionName;
     }
 
